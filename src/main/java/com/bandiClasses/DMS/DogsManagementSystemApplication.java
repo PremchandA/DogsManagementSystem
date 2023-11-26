6a1314a42151f0bc90a2e0b2356acc469f795a98
PremchandA <123143572+PremchandA@users.noreply.github.com>
2023-11-25 20:26:19 -0600

    Added change in main program

--- src/main/java/com/bandiClasses/DMS/DogsManagementSystemApplication.java
+++ src/main/java/com/bandiClasses/DMS/DogsManagementSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DogsManagementSystemApplication {
 
+ // Added change
 	public static void main(String[] args) {
 		SpringApplication.run(DogsManagementSystemApplication.class, args);
 	}
